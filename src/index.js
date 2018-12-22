7f1ac81e486292b38a40a35955e098bb72a096b1
Gregory Iannacone <gregoryiannacone@gmail.com>
2018-12-21 22:28:30 -0800

    Initial css

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// Libraries
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Components
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+// CSS
+import "./index.css";
+import "./css/App.css";
+import "./css/Registration.css";
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
