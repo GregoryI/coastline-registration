5144bad726b0e20247500cdf6f33091f27660980
Gregory Iannacone <gregoryiannacone@gmail.com>
2018-12-23 03:33:37 -0800

    Accounting (+ EFT/Credit Card) select options

--- src/util/registrationSelectOptions.js
+++ src/util/registrationSelectOptions.js
@@ -25,3 +25,22 @@ export const DELIVERIES_TO = [
   { value: "3:00PM", label: "3:00PM" },
   { value: "4:00PM", label: "4:00PM" }
 ];
+
+export const PAYMENT_METHODS = [
+  { value: "EFT (DIRECT DEBIT)", label: "EFT (DIRECT DEBIT)" },
+  { value: "CREDIT CARD", label: "CREDIT CARD" },
+];
+
+export const ACCOUNTING_TERMS = [
+  { value: "CHARGE ON DELIVERY", label: "CHARGE ON DELIVERY" },
+  { value: "NET 7", label: "NET 7" },
+  { value: "NET 14", label: "NET 14" }
+];
+
+export const INSTITUTIONS = [
+  { value: "Royal Bank of Canada (RBC)", label: "Royal Bank of Canada (RBC)" },
+  { value: "Toronto-Dominion Bank (TD)", label: "Toronto-Dominion Bank (TD)"},
+  { value: "Bank of Nova Scotia (Scotiabank)", label: "Bank of Nova Scotia (Scotiabank)"},
+  { value: "Bank of Montreal (BMO)", label: "Bank of Montreal (BMO)"},
+  { value: "Canadian Imperial Bank of Commerce (CIBC)", label: "Canadian Imperial Bank of Commerce (CIBC)" }
+];
